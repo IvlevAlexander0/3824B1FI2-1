454f95cdd3a812c2ba89290e1c783acf0d9104c6
Mercurial00 <danil.starostin.2006@mail.ru>
2024-09-06 23:40:31 +0300

    fixed

--- StarostinD/Lab0/Lab0.c
+++ StarostinD/Lab0/Lab0.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-#include <locale.h>
 
 int check(int x1, int y1, int r1, int x2, int y2, int r2) {
 	double s = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
@@ -17,31 +16,30 @@ int check(int x1, int y1, int r1, int x2, int y2, int r2) {
 }
 
 int main() {
-	setlocale(LC_ALL, "Rus");
 	int x1, y1, r1 = 0, x2, y2, r2 = 0;
-	printf("Введите информацию о первой окружности: ");
+	printf("Enter information about the first circle: ");
 	while (r1 == 0) {
 		scanf_s("%d%d%d", &x1, &y1, &r1);
 		if (r1 == 0) {
-			printf("Ошибка! Радиус не может быть равен 0. Введите корректную информацию.\n");
+			printf("Error! Radius can't be equal to 0. Please enter correct information.\n");
 		}
 	}
-	printf("Введите информацию о второй окружности: ");
+	printf("Enter information about the second circle: ");
 	while (r2 == 0) {
 		scanf_s("%d%d%d", &x2, &y2, &r2);
 		if (r2 == 0) {
-			printf("Ошибка! Радиус не может быть равен 0. Введите корректную информацию.\n");
+			printf("Error! Radius can't be equal to 0. Please enter correct information.\n");
 		}
 	}
 	switch (check(x1, y1, r1, x2, y2, r2)) {
 	case(1):
-		printf("Окружности не пересекаются\n");
+		printf("Circles don't intersect\n");
 		break;
 	case(2):
-		printf("Окружности касаются\n");
+		printf("Circles touch\n");
 		break;
 	case(3):
-		printf("Окружности пересекаются\n");
+		printf("Circles intersect\n");
 		break;
 	default:
 		break;
