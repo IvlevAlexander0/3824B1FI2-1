17a6c4f7bd4062ed8ddd75ba8ef994c5b4a83093
Nikita <serbakovnikita49@gmail.com>
2024-09-23 21:12:59 +0300

    Lab1NSV2

--- ShcherbakovN/Lab1/Lab1.c
+++ ShcherbakovN/Lab1/Lab1.c
@@ -4,15 +4,23 @@
 int main() {
 	float h, w, d, depth1 = 1.5, depth2 = 0.5, depth3 = 1.0, V_back, V_side, V_up_down, V_doors, V_shelfs, m_all;
 	float p_dsp = 0.00065, p_dvp = 0.0008, p_wood = 0.00055;
+	char a;
 	printf("Enter the height, width and depth for the wardrobe (in centimeters): ");
-	if (scanf_s("%f %f %f", &h, &w, &d) != 3) {
-		return -1;
-		exit(-1);
-	}
-	else if (h < 180.0 || h>220.0 || w < 80.0 || w>120.0 || d < 50.0 || d>90.0) {
-		return -2;
-		exit(-2);
-	}
+	do
+	{
+		a = scanf_s("%f %f %f", &h, &w, &d);
+		printf("-----\n");
+		if (a != 3) 
+		{
+			char c;
+			while ((c = getchar()) != '\n' && c != EOF)
+			{
+				c = scanf_s("%f %f %f", &h, &w, &d);
+				printf("-----\n");
+			}
+		}
+	} 
+	while (h < 180.0 || h > 220.0 || w < 80.0 || w > 120.0 || d < 50.0 || d > 90.0);
 	V_back = h * w * depth2;
 	V_side = h * d * depth1 * 2;
 	V_up_down = w * d * depth1 * 2;
