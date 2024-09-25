f2cc2e9ae6f04b8014f93dfd8a03f97e6d9166b8
Kirill_Krasnov <krasnovkirill62@gmail.com>
2024-09-26 00:13:31 +0300

    changes

--- KrasnovKirill/Lab1/Practice1/Source.cpp
+++ KrasnovKirill/Lab1/Practice1/Source.cpp
@@ -4,28 +4,35 @@
 int main() {
 
 	float h, w, d, m, dvp = 820, dsp = 800, wood = 650, m_back, m_sides, m_ud, m_doors, m_sh;
+	int flag, c_sh = 0;
 	/* h - height; w - width; d - depth; m_* - mass of cupboard part; dvp, dsp, wood - material density(kg/m^3)*/
-	do {
+	flag = 1;
+	while (flag == 1) {
 		printf("Input height (180-220), width(80-120), depth (50-90) of the cupboard(cm):");
 		scanf_s("%f %f %f", &h, &w, &d);
-		if ((h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90)) {
+		if ((h >= 180) && (h <= 220) && (w >= 80) && (w <= 120) && (d >= 50) && (d <= 90)) {
+			flag = 0;
+		}
+		else {
 			printf("Uncorrect data\n");
 		}
 		char c;
 		while ((c = getchar()) != '\n' && c != EOF) {}
-	} while ((h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90)); // still didn't get how to use "flag" :)
+	}
 	
 	m_back = h * w * 0.5 * dvp;
 	m_sides = h * d * 1.5 * dsp * 2;
-	m_ud = (w - 3) * (d - 0.5) * 1.5 * dsp * 2;
+	m_ud = w * d * 1.5 * dsp * 2;
 	m_doors = h * w * wood;
 
-	if (h > 200) {
-		m_sh = (w - 3) * (d - 0.5) * 1.5 * dsp * 5;
-	} // +1 shelf
+	if (h > 210) {
+		m_sh = w * d * 1.5 * dsp * 5;
+		c_sh += 1;
+	} // +1 shelf (enough place to add one more shelf)(10-20cm free place if  210-220cm)	|	1(40)	2(80)	3(120)	4(160)	5(200)  |
 	else {
-		m_sh = (w - 3) * (d - 0.5) * 1.5 * dsp * 4;
-	} // +0 shelf
+		m_sh = w * d * 1.5 * dsp * 4;
+	} // +0 shelf (not enough place for one shelf)	|	1(40)	2(80)	3(120)	4(160)	|
+
 	m = (m_back + m_sides + m_ud + m_doors + m_sh) / 1000000;
 
 	printf("m = %f kg", m);
