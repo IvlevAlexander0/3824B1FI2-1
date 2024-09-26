bec86b5a0ddc6de29491e237110051f7a3378b98
Kirill_Krasnov <krasnovkirill62@gmail.com>
2024-09-26 10:46:35 +0300

    changes

--- KrasnovKirill/Lab1/Practice1/Source.cpp
+++ KrasnovKirill/Lab1/Practice1/Source.cpp
@@ -6,13 +6,11 @@ int main() {
 	float h, w, d, m, dvp = 820, dsp = 800, wood = 650, m_back, m_sides, m_ud, m_doors, m_sh;
 	int flag = 1;
 	/* h - height; w - width; d - depth; m_* - mass of cupboard part; dvp, dsp, wood - material density(kg/m^3)*/
-	while (flag == 1) {
+	while (flag) {
 		printf("Input height (180-220), width(80-120), depth (50-90) of the cupboard(cm):");
 		scanf_s("%f %f %f", &h, &w, &d);
-		if ((h >= 180) && (h <= 220) && (w >= 80) && (w <= 120) && (d >= 50) && (d <= 90)) {
-			flag = 0;
-		}
-		else {
+		flag = (h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90);
+		if (flag) {
 			printf("Uncorrect data\n");
 		}
 		char c;
@@ -24,7 +22,7 @@ int main() {
 	m_ud = w * d * 1.5 * dsp * 2;
 	m_doors = h * w * wood;
 
-	if (h > 210) {
+	if (h >= 210) {
 		m_sh = w * d * 1.5 * dsp * 5;
 	} // +1 shelf (enough place to add one more shelf)(10-20cm free place if  210-220cm)	|	1(40)	2(80)	3(120)	4(160)	5(200)  |
 	else {
