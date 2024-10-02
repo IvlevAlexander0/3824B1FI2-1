33a5062cad3ecadb94237bdac4dafe81edc602e2
Vlad <vladislav.student.08@mail.ru>
2024-10-02 09:56:15 +0300

    changes

--- BolshakovV/Lab 1.1/Source.c
+++ BolshakovV/Lab 1.1/Source.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 int main() {
 	double h, w, d; /*h-height, w-width, d-depth*/
 	double sten, bok, kr, ndr, polka;
-	double p1, p2, p3 /*p1 - dwp, p2 - dsp, p3- wood*/;
+	double dwp, dsp, wood;
 	int flag;
 	int c;
-	p1 = 0.82;
-	p2 = 0.8;
-	p3 = 0.6;
+	dwp = 0.82;
+	dsp = 0.8;
+	wood = 0.6;
 	double M;
 	int n;
 	printf("Get ready to enter the values\n");
@@ -16,20 +15,27 @@ int main() {
 	do {
 		flag = 0;
 		int r = scanf("%lf %lf %lf", &h, &w, &d);
-		if (r != 3) {
+		if ((r != 3) || (h < 180.0 || h > 220.0 || w < 80.0 || w > 120.0 || d < 50.0 || d > 90.0)) {
 			flag = 1;
 			printf("Error. Please enter the correct values\n");
 		}
-		while ((c = getchar()) != '\n' && c != EOF) 
-		{}
+		while ((c = getchar()) != '\n' && c != EOF)
+		{
+		}
 	} while (flag != 0);
 
-	sten = h * w * 0.5 * p1;
-	bok = 2 * h * d * 1.5 * p2;
-	kr = 2 * w * d * 1.5 * p2;
-	ndr = 2 * h * w * 1 * p3;
-	n = h / 40;
-	polka = n * w * d * 1 * p2;
+	sten = h * w * 0.5 * dwp;
+	bok = 2 * h * d * 1.5 * dsp;
+	kr = 2 * w * d * 1.5 * dsp;
+	ndr = 2 * h * w * 1 * wood;
+	n = (h - 3) / (40 + 1);
+
+	/*	if (h < 208) {
+		n = 4;}
+		else { n = 5 ; } */
+
+	//printf("%d\n", n);
+	polka = n * (w - 3) * d * 1 * dsp;
 	M = sten + bok + kr + ndr + polka;
 	printf("Weight in grams = %f", M);
 	return 0;
