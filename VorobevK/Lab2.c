d8657408c472aed9abc13f080c01d68bd40e74af
VorobevKonstantin <kostyahard222@ya.ru>
2024-10-06 22:52:47 +0300

    minor_optimizations_and_added_counter

--- VorobevK/Lab2.c
+++ VorobevK/Lab2.c
@@ -4,22 +4,22 @@
 //minimal user interface
 int main(){
     //printf("%d",RAND_MAX); 32767
-    int mode;
-    char c;
+    int mode, counter=0;
+    char c;//easier to create c and counter here instead of inside each mode
     srand(time(NULL));//obviously "inspired" by internet, but actually works
     scanf("%d", &mode); //assuming all inputs are valid
     if(mode==1){
-        int inp;
-        int r = rand()%1000;
+        int inp, r = rand()%1000;
         do{
             scanf("%d",&inp);//they are all valid, right? Right...?
+            counter+=1;
             while((c=getchar())!='\n' && c!=EOF){}//works even w/o it, but better be safe than sorry
             if(inp>r){
                 printf("take lower\n");
             }else if(inp<r){
                 printf("take higher\n");
             }else{
-                printf("Correct!\n");
+                printf("Correct! %d tries",counter);
                 return 0;//exit(0) but not in pyhton
             }
         }while(1);
@@ -36,7 +36,9 @@ int main(){
             }else{//probably would be better if i'd put a checker that it actually IS a '<' but works anyway if user isn't a moron
                 topborder=currentguess;
             }
+            counter+=1;
         }while(inp!='=');
+        printf("%d tries",counter);
         return 0;//exit(0) but i have no idea where it sends this zero
     }
 }
