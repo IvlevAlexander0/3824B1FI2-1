7af5b1ff4287f774bc3f631d449531bbaf3e3cc7
para5ol <an02.02@mail.ru>
2024-10-07 21:29:01 +0300

    changes_made

--- NenevA/Lab№2/Lab№2/Lab№2.cpp
+++ NenevA/Lab№2/Lab№2/Lab№2.cpp
@@ -1,34 +1,39 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 int main() {
 
-	int n, mode, guess, lim1, lim2, scz, flg = 1;
-	char odp[2], eq[] = "=", mo[] = ">", le[] = "<";
+	int n, mode, guess, scz;
 	scz = 0;
-	lim1 = 1;
-	lim2 = 1000;
 
 	printf("Enter the program's mode (1 - you guess the program's number; 2 - program guesses your number)\n");
 	scanf_s("%d", &mode);
 
 	if (mode == 1)
 	{
+		srand(time(NULL));
 		n = 1 + rand() % 1000;
-		printf("Try to guess program's number\n");
-		scanf_s("%d", &guess);
+
+		do {
+			printf("Try to guess program's number entering the number from 1 to 1000\n");
+			scanf_s("%d", &guess);
+		} while (guess<1 || guess>1000);
 
 		while (guess != n) {
 
 			if (guess > n) {
-				printf("Your number is too large. Try again.\n");
+				printf("Your number is too large. ");
 			}
 
 			else {
-				printf("You number is too small. Try again.\n");
+				printf("You number is too small. ");
 			}
 
-			scanf_s("%d", &guess);
+			do {
+				printf("Try to guess program's number entering the number from 1 to 1000\n");
+				scanf_s("%d", &guess);
+			} while (guess < 1 || guess>1000);
 			scz++;
 		}
 		printf("You've guessed! Program's number is %d\n", guess);
@@ -37,11 +42,20 @@ int main() {
 
 	else if (mode == 2) {
 
-		guess = 0;
-		printf("Enter your number and the program will try to guess it\n");
-		scanf_s("%d", &n);
+		char odp[2], eq[] = "=", mo[] = ">", le[] = "<";
+		int lim1, lim2;
 
-		while (flg) {
+		lim1 = 1;
+		lim2 = 1000;
+
+		guess = 0;
+
+		do {
+			printf("Enter your number(from 1 to 1000) and the program will try to guess it\n");
+			scanf_s("%d", &n);
+		} while (n < 1 || n>1000);
+
+		while (1) {
 			fgets(odp, sizeof(odp), stdin);
 			if (strcmp(odp, mo) == 0) {
 				lim1 = guess;
@@ -54,8 +68,7 @@ int main() {
 			}
 
 			else if (strcmp(odp, eq) == 0 && guess == n) {
-				printf("The program have guessed\n");
-				flg = 0;
+				printf("The program has guessed\n");
 				break;
 			}
 
