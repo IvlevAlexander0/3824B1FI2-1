3c87c88bf9754b78d22c82f3e5daba761c3dcf51
Vlad <vladislav.student.08@mail.ru>
2024-10-11 15:40:06 +0300

    change

--- BolshakovV/Lab 2.1/Source.c
+++ BolshakovV/Lab 2.1/Source.c
@@ -5,35 +5,46 @@
 int main() {
 	printf("Please select the mode\n");
 	printf("1 -  You're guessing, 2 - The computer guesses\n");
-	int x, flag, c;
-	do {
-		flag = 0;
-		int r = scanf("%d", &x);
-		if ((r != 1) || (x < 1) || (x > 2)) {
-			flag = 1;
-			printf("Error. Please enter the correct values\n");
+	int x;
+	srand(time(NULL));
+	while (1) {
+		int r = scanf_s("%d", &x);
+		if (r == 1 && (x == 1 || x == 2)) {
+			break;
 		}
-		while ((c = getchar()) != '\n' && c != EOF) 
-		{}
-	} while (flag != 0);
-
+		else {
+			printf("Error. Please enter the correct values\n");
+			while (getchar() != '\n');
+		}
+	}
 	if (x == 1) {
-		srand(time(NULL));
 		int r, range, n, count;
 		count = 0;
-		range = 1001;
-		r = rand() % range;
+		range = 1000;
+		r = 1 + rand() % (range);
+		printf("Please enter a value in the range [1;1000]\n");
 		//printf("%d", r);
-		scanf_s("%d", &n);
+		while (1) {
+			int m = scanf_s("%d", &n);
+			if (m == 1 && (n <= 1000 && n >= 1)) {
+				break;
+			}
+			else {
+				printf("Error. Please enter the correct values\n");
+				while (getchar() != '\n');
+			}
+		}
 		count += 1;
 		while (r != n) {
 			if (r > n) {
 				printf("The hidden number is bigger\n");
+				printf("Please enter a value in the range [1;1000]\n");
 				scanf_s("%d", &n);
 				count += 1;
 			}
 			if (r < n) {
 				printf("The hidden number is lessen\n");
+				printf("Please enter a value in the range [1;1000]\n");
 				scanf_s("%d", &n);
 				count += 1;
 			}
@@ -44,52 +55,43 @@ int main() {
 		}
 	}
 	if (x == 2) {
-		srand(time(NULL));
-		int a, b, k, range;
-		int count2;
-		count2 = 0;
-		char m = '0';
-		range = 1001;
-		printf(" > - the hidden number is bigger; < - the hidden number is lessen; = -  the number are equal\n");
-		scanf_s("%d", &a);
-		b = rand() % range;
-		printf("%d", b);
-		k = abs(a - b);
+		int min = 1;
+		int max = 1000;
+		int b;
+		int count = 0;
+		char c;
+		while (min <= max) {
+			b = min + (max - min) / 2;
+			count++;
 
-		scanf_s("\n%c", &m);
-		count2 += 1;
-		while (m != '=') {
-			if (m == '<' && k > a) {
-				k = abs(b - a);
-				b = rand() % k;
-				printf("%d", b);
-				scanf_s("\n%c", &m);
-				count2 += 1;
-
+			printf("%d\n", b);
+			printf("Enter (>, <, =); > - the hidden number is bigger, < - the hidden number is lessen; = - the number are equal\n");
+			while (1) {
+				scanf_s(" %c", &c);// when using <getchar> two character inputs are required (example: 500: >>: new number 750)
+				if (c == '>' || c == '<' || c == '=') {
+					break;
+				}
+				else {
+					printf("Error. Please enter the correct values\n");
+					while (getchar() != '\n');
+				}
 			}
-			if (m == '<' && k < a) {
-				k = abs(b - a);
-				b = k + rand() % (a - k + 1);
-				printf("%d", b);
-				scanf_s("\n%c", &m);
-				count2 += 1;
 
-
+			if (c == '=') {
+				printf("Win!!!\n");
+				printf("%d %d\n", b, count);
+				break;
 			}
-			if (m == '>') {
-				k = b;
-				b = k + rand() % (a - k + 1);
-				printf("%d", b);
-				scanf_s("\n%c", &m);
-				count2 += 1;
-
+			else if (c == '>') {
+				min = b + 1;
+			}
+			else if (c == '<') {
+				max = b - 1;
 			}
 		}
-		if (m == '=') {
-			printf("WIN!!!\n");
-			printf("%d", count2);
+		if (min > max) {
+			printf("Error. The entered value is not in range [1 - 1000] \n");
 		}
-
 	}
 
 	return 0;
