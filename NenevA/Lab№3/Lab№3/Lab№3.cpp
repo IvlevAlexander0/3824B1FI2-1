bcceb0184443362ccc85ff6e551b6c41d2d8dcda
para5ol <an02.02@mail.ru>
2024-10-27 13:08:35 +0300

    mistakes_corrected

--- NenevA/Lab№3/Lab№3/Lab№3.cpp
+++ NenevA/Lab№3/Lab№3/Lab№3.cpp
@@ -5,16 +5,14 @@
 
 int main() {
 	int List[5] = { -1,-1,-1,-1,-1 };
-	int i = 0, n, num, j, flg;
+	int i = 1, n, num, j, flg;
 	srand(time(NULL));
-	List[0] = 1 + rand() % 9;
+	List[0] = 1 + rand() % 9; // Теперь, когда i начинается с 1 первая цифра(List[0]) всегда больше 0
 	do {
 		printf("Enter the number's length(from 2 to 5)\n");
 		scanf_s("%d", &n);
 	} while (n < 2 || n>5);
-	printf("Please wait a little bit while the number is being generated\n");
 	while (i < n) {
-		srand(time(NULL));
 		num = rand() % 10;
 		flg = 1;
 		for (j = 0; j < n; j++) {
@@ -27,7 +25,6 @@ int main() {
 			i++;
 		}
 	}
-
 	int guess, save;
 	printf("Try to guess the number\n");
 	while (1) {
@@ -41,12 +38,13 @@ int main() {
 			scanf_s("%d", &guess);
 			save = guess;
 			i = 0;
-			while (save > 0) {
-				ListTry[i] = save % 10;
+			int scz = n;
+			while (save > 0 && scz > 0) {
+				ListTry[n - i - 1] = save % 10;
 				save /= 10;
 				i++;
+				scz--;
 			}
-
 			for (i = 0; i < n - 1; i++) {
 				for (j = i + 1; j < n; j++) {
 					if (ListTry[i] == ListTry[j]) {
