6b17ea77df5506370be4363f78284aa04a6c6acb
Alibidercha <daniiltsipin@gmail.com>
2024-11-12 23:26:12 +0300

    fix

--- TsipinD/Lab№3/1.cpp
+++ TsipinD/Lab№3/1.cpp
@@ -29,29 +29,36 @@ int main() {
 	//for (int i = 0; i < length; i++) {
 	//	printf_s("%d ", digits[i]);}
 	int positions = 0, counter;
-	char input[12];
 	int valid;
 	int* digits_ask = (int*)malloc(length * sizeof(int));
 	char ch;
+
 	ch = getchar(); // без этой строки fgets при первом запуске цикла считает \n и сразу напишет ошибку о неправильной длине числа
 	while (positions != length) {
 		valid = 0;
 		while (!valid) { // алгоритм проверки длины и уникальности цифр вводимого числа
 			printf_s("Enter the number: ");
-			fgets(input, sizeof(input), stdin); 
-			int num_digits = strlen(input) - 1; // т.к. последний символ это переход на новую строку
-			if (num_digits != length) {
+			char* input = NULL;
+			int count = 0;
+			do {
+				scanf_s("%c", &ch);
+				if (ch == '\n')
+					break;
+				input = (char*)realloc(input, ++count * sizeof(char));
+				input[count - 1] = ch;
+			} while (1);
+			if (count != length) {
 				printf("Your number must be %d in length.\n\n", length);
 				continue; 
 			}
 
 			int unique = 1; // проверка на различность всех цифр числа
-			for (int i = 0; i < num_digits; i++) {
+			for (int i = 0; i < count; i++) {
 				if (input[i] < '0' || input[i] > '9') { 
 					unique = 0;
 					break;
 				}
-				for (int j = i + 1; j < num_digits; j++) {
+				for (int j = i + 1; j < count; j++) {
 					if (input[i] == input[j]) {
 						unique = 0;
 						break;
@@ -70,6 +77,7 @@ int main() {
 				digits_ask[i] = input[i] - '0';
 			}
 			valid = 1;
+			free(input);
 		}
 		positions = 0;
 		counter = 0;
